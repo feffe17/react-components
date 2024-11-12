22a462313d73a1a27577341d579482e4b767c60c
feffe1702 <fpm.asd@gmail.com>
2024-11-12 15:02:49 +0100

    [WIP] complete header + main

---
+++ src/assets/AppFooter.jsx
@@ -0,0 +1,12 @@
+export default function AppFooter() {
+
+    const intro = "welcome to my react app"
+    return (
+        <footer>
+            <div className="intro">
+                <h2>footer</h2>
+            </div>
+        </footer>
+    )
+
+}
---
+++ src/assets/AppMain.jsx
@@ -0,0 +1,19 @@
+export default function AppMain() {
+
+
+    return (
+        <main>
+            <div className="container">
+                <div className="card">
+                    <div className="top-section">600x400</div>
+                    <div className="bot-section">
+                        <h5>Titolo del post</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam suscipit eius quidem, accusantium, corrupti nisi facilis quis libero molestias et veritatis asperiores saepe praesentium voluptates. Ex quidem laudantium ratione placeat!</p>
+                        <button>Leggi di più</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+
+}
