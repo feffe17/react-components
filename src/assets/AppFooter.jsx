da5680d574a64044436180649b785710cdcd4435
feffe1702 <fpm.asd@gmail.com>
2024-11-12 15:17:11 +0100

    [WIP] complete footer

--- src/assets/AppFooter.jsx
+++ src/assets/AppFooter.jsx
@@ -3,8 +3,8 @@ export default function AppFooter() {
     const intro = "welcome to my react app"
     return (
         <footer>
-            <div className="intro">
-                <h2>footer</h2>
+            <div className="credits">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut neque alias natus quas vitae, accusamus, eius molestias atque odio voluptatibus labore fuga? Obcaecati repellendus enim eius optio cumque amet quae.
             </div>
         </footer>
     )
