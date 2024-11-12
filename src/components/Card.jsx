4844eeb261d24965d025f56cc8d8a6bfbf5d4316
feffe1702 <fpm.asd@gmail.com>
2024-11-12 18:44:23 +0100

    [DONE] create external components for css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
-
+import style from "./Card.module.css";
 
 export default function Card({ title, description, buttonText }) {
     return (
-        <div className="card">
-            <div className="top-section">600x400</div>
-            <div className="bot-section">
-                <h5>{title}</h5>
-                <p>{description}</p>
-                <button>{buttonText}</button>
+        <div className={style.card}>
+            <div className={style.topSection}>600x400</div>
+            <div className={style.botSection}>
+                <h5 className={style.h5}>{title}</h5>
+                <p className={style.p}>{description}</p>
+                <button className={style.button}>{buttonText}</button>
             </div>
         </div>
     );
